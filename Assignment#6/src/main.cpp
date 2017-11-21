ef5eac3d5ae1c5c7564cb9036de17956ac62c085
Dharini Sreenivasan <dharini.09@gmail.com>
2017-11-20 19:22:26 -0800

    Modified sampling frequency and csv files

--- Assignment#6/src/main.cpp
+++ Assignment#6/src/main.cpp
@@ -26,28 +26,18 @@ float y[BATCH + 1];
 float z[BATCH + 1];
 String ppg_data[BATCH + 1];
 
-void setup()
-{
+void setup()    {
     debug.begin(115200);
     debug.println("Initializing...");
 
     // Initialize paritcle sensor
-    //Use default I2C port, 400kHz speed
+    // Use default I2C port, 400kHz speed
     if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) {
         debug.println("MAX30105 was not found. Please check wiring/power. ");
-        while (1)
-            ;
+        while (1);
     }
 
-    //LED Pulse Amplitude Configuration
-    //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-    //Default is 0x1F which gets us 6.4mA
-    //powerLevel = 0x02; // 0.4mA - Presence detection of ~4 inch
-    //powerLevel = 0x1F, 6.4mA - Presence detection of ~8 inch
-    //powerLevel = 0x7F, 25.4mA - Presence detection of ~8 inch
-    //powerLevel = 0xFF, 50.0mA - Presence detection of ~12 inch
-
-    byte sampleRate = 50; //Sample at 50 Hz
+    byte sampleRate = 250; //Sample at 250 Hz
     byte ledBrightness = 0xFF; //Options: 0=Off to 255=50mA
     byte sampleAverage = 4; //Options: 1, 2, 4, 8, 16, 32
     byte ledMode = 2; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
@@ -60,7 +50,8 @@ void setup()
     particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange);
 
     // Initialize accelerometer
-    myIMU.settings.accelSampleRate = 50;
+    myIMU.settings.accelSampleRate = 250;
+    myIMU.settings.accelRange = 16;
     myIMU.begin();
 
     // WiFi setup
@@ -74,12 +65,10 @@ void setup()
     end += start;
 }
 
-void sendDataToServer(int offset)
-{
+void sendDataToServer() {
     String accelerometer_data = "lis3dh=";
     String particle_sensor_data = "max30105=";
-
-    if (WiFi.status() == WL_CONNECTED) {
+    
         for (int i = 0; i < BATCH; ++i) {
             accelerometer_data = accelerometer_data + x[i] + "," + y[i] + "," + z[i] + "\n";
             particle_sensor_data = particle_sensor_data + ppg_data[i] + "\n";
@@ -94,12 +83,9 @@ void sendDataToServer(int offset)
         http.POST(particle_sensor_data);
         http.writeToStream(&Serial);
         http.end();
-    }
-    delay(2000);
 }
 
-void loop()
-{
+void loop() {
     // Gather the IR and RED data, and accelerometer data
     if (WiFi.status() == WL_CONNECTED) {
         if (millis() < end) {
@@ -115,15 +101,14 @@ void loop()
             count++;
 
             if ((batchCount % BATCH) == 0) {
-                int offset = count - BATCH;
-                sendDataToServer(offset);
+                sendDataToServer();
                 batchCount = 0;
             }
         }
-    }
-    else {
-        Serial.println("End");
-        delay(1000);
-        wdt_reset();
+        else {
+            Serial.println("End");
+            delay(1000);
+            wdt_reset();
+        }
     }
 }
