7536f26f36008075f785cad6009b37f0a8f5c7f2
Dharini Sreenivasan <dharini.09@gmail.com>
2017-11-27 14:48:09 -0800

    Initial commit for Assignment 7

---
+++ Assignment#7/calc_readings/main.py
@@ -0,0 +1,161 @@
+#
+# Team 4 - Assignment 6
+#
+import numpy as np
+
+from scipy.signal import butter, lfilter
+import matplotlib.pyplot as plt
+
+import math
+
+data = np.genfromtxt("assignment6_max30105.csv",delimiter=",", skip_header=1, dtype=float)
+formattedData = np.empty((data.shape[0], 3))
+formattedData[:,:] = data[:,0:3]
+
+def findMaxMins(data):
+
+    minimas = []
+    maximas = []
+
+    for i in range(1, len(data)-1):
+        if (data[i-1] < data[i]) and (data[i] > data[i+1]):
+            maximas.append(i)
+        if (data[i-1] > data[i]) and (data[i] < data[i+1]):
+            minimas.append(i)
+    return maximas, minimas
+
+def butterworthLowpass(cutoff, sample_rate, order=5):
+    filterCutoff = cutoff / (0.5 * sample_rate)
+    return butter(order, filterCutoff, analog=False, btype='low')
+    
+#Butterworth low pass filter
+def butterworthLowpassFilter(data, cutoff, sample_rate, order=5):
+    b, a = butterworthLowpass(cutoff, sample_rate, order=order)
+    return lfilter(b, a, data)
+
+
+def butterworth(lowThreshold, highThreshold, sample_rate, order=5):
+    nyquist = 0.5 * sample_rate
+    low = lowThreshold / nyquist
+    high = highThreshold / nyquist
+    return butter(order, [low, high], btype='band')
+
+#Butterworth bandpass filter
+def butterworthFilter(data, lowThreshold, highThreshold, sample_rate, order=5):
+    b, a = butterworth(lowThreshold, highThreshold, sample_rate, order=order)
+    return lfilter(b, a, data)
+
+
+def filterBoundaries(lowBpm, highBpm, freqs):
+    higherThreshold = float(highBpm)/60.0   
+    lowerThreshold = float(lowBpm)/60.0 
+
+    start = -1
+    for i in range(len(freq)):
+        if freqs[i] >= lowerThreshold:
+            start = i
+            break
+
+    end = -1
+    for i in range(len(freq)):
+        if freqs[i] >= higherThreshold:
+            end = i
+            break
+
+    return start, end
+
+def findPeak(fftvalue, start, end):
+    peak = 0.0
+    peakFreq = -1
+    for i in range(start, end):
+        if fftvalue[i-1]<fftvalue[i]>fftvalue[i+1] and fftvalue[i] > peak:
+            peak = fftvalue[i]
+            peakFreq = i
+    return peakFreq
+
+def calHeartRate(fftvalue, freqs):
+    start, end = filterBoundaries(40.0, 220.0, freqs)
+    peakFreq = findPeak(fftvalue, start, end)
+    return freqs[peakFreq]
+
+def calRespiratoryRate(fftvalue, freqs):
+    start, end = filterBoundaries(8, 22, freqs)
+    peakFreq = findPeak(fftvalue, start, end)
+    return freqs[peakFreq]
+
+
+#Problem statement solving
+heartRate = 0.0
+respRate = 0.0
+spo2Value = 0.0
+
+
+#Given as 50Hz
+sampleFreq = 50
+
+dataSignal = formattedData[:,2]
+
+timestep = 1.0/sampleFreq
+splits = 1
+
+
+# Calculating SpO2
+IR = data[:, 2]
+RED = data[:, 1]
+IR = butterworthLowpassFilter(IR, 2, sampleFreq, 6)[2500:]
+RED = butterworthLowpassFilter(RED, 2, sampleFreq, 6)[2500:]
+xValues = [_ for _ in range(len(IR))]
+redMaximums, redMinimums = findMaxMins(RED)
+irMaximums, irMinimums = findMaxMins(IR)
+redYValues = [RED[i] for i in redMinimums]
+redYInterp = np.interp(xValues, redMinimums, redYValues)
+irYValues = [IR[i] for i in irMinimums]
+irYInterp = np.interp(xValues, irMinimums, irYValues)
+acIr = [(IR[i] - irYInterp[i]) for i in irMaximums]
+dcIr = [irYInterp[i] for i in irMaximums]
+acRed = [(RED[i] - redYInterp[i]) for i in irMaximums]
+dcRed = [redYInterp[i] for i in irMaximums]
+spo2ans = []    
+
+
+for i in range(len(acRed)):
+    ratioAverage = (acRed[i]*dcIr[i])/(acIr[i]*dcRed[i])
+    if ratioAverage >1:
+        continue
+    spo2 = (-45.060* ratioAverage * ratioAverage) + (30.354 * ratioAverage) + 94.845
+    if spo2>0:
+        spo2ans.append(spo2)
+from collections import Counter
+counts = Counter(spo2ans)
+maxCountTuple = counts.most_common(1)
+spo2CleanedAns = filter(lambda a:a !=maxCountTuple[0][0], spo2ans)
+
+
+spo2Value = np.mean(spo2CleanedAns)
+print "The calculated SpO2:- ", spo2Value
+
+
+#Resp and Heart rate
+for item in np.array_split(dataSignal, splits):
+    avg = np.mean(item)
+    item = item-avg
+    fourier = np.fft.fft(item)
+    absoluteVal = abs(fourier)**2
+    freq = np.fft.fftfreq(item.size, d=timestep)
+    heartRateFreq = calHeartRate(absoluteVal, freq)
+    filteredDataSignal = butterworthFilter(item, heartRateFreq-0.5, heartRateFreq+0.5, sampleFreq)    
+    maximums, mins = findMaxMins(filteredDataSignal[5000:])
+    heartrateList = [(60*50/float(maximums[i]-maximums[i-1])) for i in range(1, len(maximums))]
+    heartRate += np.mean(heartrateList)/splits
+    
+
+    respRate_freq = calRespiratoryRate(absoluteVal, freq)
+    filteredDataSignal = butterworthFilter(item, 0.2, 0.4, sampleFreq, order = 4)    
+    maximums, mins = findMaxMins(filteredDataSignal[5000:])
+    resprate_arr = [(60*50/float(maximums[i]-maximums[i-1])) for i in range(1, len(maximums))]
+    resprate_arr = filter(lambda x: 6<x<24,  resprate_arr)
+    respRate += np.mean(resprate_arr)/splits
+
+
+print "The calculated Respiration Rate:- ", respRate
+print "The calculated Heart Rate:- ", heartRate
---
+++ Assignment#7/src/main.cpp
@@ -0,0 +1,193 @@
+/**
+ * Program to gather PPG sensor and accelerometer data using the ESP8266 board,
+ * store the data in memory and send it to the server
+ * 
+ * Created on 26 Nov 2017
+ * By dharini-s
+ */
+#include "MAX30105.h"
+#include "Arduino.h"
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <Wire.h>
+#include "SparkFunLIS3DH.h"
+#include "SPI.h"
+#include "FS.h"
+
+MAX30105 particleSensor;
+LIS3DH myIMU;
+
+#define debug Serial
+
+// const char *ssid = "UCInet Mobile Access";
+const char *ssid = "26D-Comrades2.4";
+const char *password = "AisDhaDivHarNit26D";
+const String URL = "http://54.183.180.162/assignment6.php";
+
+const int BATCH = 100;
+int batchCount = 0;
+unsigned long start = 0;
+// 1 min in milliseconds
+unsigned long end = 15000;
+File f1;
+File f2;
+
+float x[BATCH + 1];
+float y[BATCH + 1];
+float z[BATCH + 1];
+String ppg_data[BATCH + 1];
+
+void setup()    {
+    debug.begin(115200);
+    debug.println("Initializing...");
+
+    // Begin SPIFFS
+    SPIFFS.begin();
+    SPIFFS.format();
+    delay(10000);
+    Serial.println("Spiffs formatted");
+
+    // Initialize paritcle sensor
+    // Use default I2C port, 400kHz speed
+    if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) {
+        debug.println("MAX30105 was not found. Please check wiring/power. ");
+        while(1);
+    }
+
+    byte sampleRate = 250; //Sample at 250 Hz
+    byte ledBrightness = 0xFF; //Options: 0=Off to 255=50mA
+    byte sampleAverage = 4; //Options: 1, 2, 4, 8, 16, 32
+    byte ledMode = 2; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
+    int pulseWidth = 411;
+    int adcRange = 2048;
+
+    particleSensor.setPulseAmplitudeRed(0x1F);
+    particleSensor.setPulseAmplitudeGreen(0x1F);
+
+    particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange);
+
+    // Initialize accelerometer
+    myIMU.settings.accelSampleRate = 300;
+    myIMU.settings.accelRange = 16;
+    myIMU.begin();
+    
+    start = millis();
+    end += start;
+}
+
+void sendDataToServer() {
+    f1 = SPIFFS.open("/f1.txt", "r");
+    f2 = SPIFFS.open("/f2.txt", "r");
+    // WiFi setup
+    // WiFi.persistent(false);
+    // WiFi.mode(WIFI_OFF);
+    // WiFi.mode(WIFI_STA);
+    if (WiFi.status() != WL_CONNECTED) {
+        WiFi.begin(ssid, password);
+    }
+
+    Serial.println("After wifi begin");
+    delay(2000);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        Serial.print("Attempting to connect...");
+    }
+    if (WiFi.status() == WL_CONNECTED) {  
+        HTTPClient http;
+        String accelerometer_data = "lis3dh=";
+        String particle_sensor_data = "max30105=";
+        String s1 = "";
+        String s2 = "";
+
+        while (f1.available())   {
+            // Serial.println("In file1 read");
+            String s = f1.readStringUntil('\n');
+            s1 += s + "\n";
+            // Serial.println(s);
+        }
+        
+        while (f2.available())   {
+            // Serial.println("In file2 read");
+            String s = f2.readStringUntil('\n');
+            s2 += s + "\n";
+            // Serial.println(s);     
+        }
+        accelerometer_data += s1;
+        Serial.println("Accelerometer data:" + accelerometer_data);
+        http.begin(URL);
+        http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+        http.POST(accelerometer_data);
+        http.writeToStream(&Serial);
+
+        particle_sensor_data += s2;
+        Serial.println("PPG data:" + particle_sensor_data);
+        http.begin(URL);
+        http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+        http.POST(particle_sensor_data);
+        http.writeToStream(&Serial);
+
+        http.end();
+        f1.close();
+        f2.close();
+    }
+    else    {
+        Serial.println("Not connected to WiFi.");
+    }
+}
+    
+void writeToFile()  {
+    // Serial.println("Before file open");
+    // Open file for writing
+    f1 = SPIFFS.open("/f1.txt", "a");
+    f2 = SPIFFS.open("/f2.txt", "a");
+    if (!f1 || !f2) {
+        Serial.println("File open failed");
+    }
+    String accelerometer_data = "";
+    String particle_sensor_data = "";
+    // Serial.println("In writeToFile()");
+
+    // write to file
+    for(int i = 0; i < BATCH; i++)  {
+        accelerometer_data = accelerometer_data + x[i] + "," + y[i] + "," + z[i] + "\n";
+        particle_sensor_data = particle_sensor_data + ppg_data[i] + "\n";
+    }
+    // Serial.print(accelerometer_data);
+    // Serial.print(particle_sensor_data);
+    // Serial.println("Before writing to file");
+    f1.print(accelerometer_data);
+    f2.print(particle_sensor_data);
+    f1.close();
+    f2.close();
+}
+
+void loop() {
+    // Gather the IR and RED data, and accelerometer data  
+        if (millis() < end) {
+            // Serial.println("Gathering data");
+            // Get the PPG sensor data
+            ppg_data[batchCount] = (String)particleSensor.getRed() + "," + (String)particleSensor.getIR();
+
+            // Get the accelerometer data
+            x[batchCount] = myIMU.readFloatAccelX();
+            y[batchCount] = myIMU.readFloatAccelY();
+            z[batchCount] = myIMU.readFloatAccelZ();
+
+            // Serial.println("X=" + (String) x[batchCount]);
+            
+            batchCount++;
+            // Serial.println("Before write to file");
+            if ((batchCount % BATCH) == 0) {
+                writeToFile();
+                batchCount = 0;
+            }
+        }
+        else {
+            sendDataToServer();
+            Serial.println("Sent data to server");
+            wdt_reset();
+            while(1)    {
+                delay(1000);
+            }
+        }
+}
